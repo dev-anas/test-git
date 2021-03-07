3caef15c3f426c53db5b7a9a606f3081fe914fe6
anass-hiit <anass.n@hiitconsulting.com>
2021-03-07 02:26:43 +0100

    first commit

---
+++ file.php
@@ -0,0 +1,17 @@
+<?php 
+class Test 
+{
+
+    public  $total;
+    public function somme(int $a, int $b)
+    {
+        $this->total = $a + $b;
+        return $this;
+    }
+
+    public function diff(int $a, int $b)
+    {
+        $this->total = $a + $b;
+        return $this;
+    }
+}
